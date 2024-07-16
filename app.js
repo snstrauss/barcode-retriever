0bcb9bf04f1ae721ea8ef7c7c5dd1d048f12d505
guyz <guy@shopic.co>
2024-07-16 11:37:49 +0300

    added server code that send request to orcascan to get barcode svg

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+const PORT = 1337;
+
+app.use(express.json());
+
+app.post("/get-barcode", (req, res) => {
+  const { code } = req.body;
+
+  if (code) {
+    requestBarcodeFromOrca(code)
+      .then((barcodeSvg) => {
+        res.send(barcodeSvg);
+      })
+      .catch((e) => {
+        errorResponse(res, e);
+      });
+  } else {
+    errorResponse(res, `'code' argument is missing!`);
+  }
+});
+
+function requestBarcodeFromOrca(code) {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`https://barcode.orcascan.com/?type=code128&data=${code}`)
+      .then((barcodeResponse) => {
+        console.log(barcodeResponse);
+        resolve(barcodeResponse.data);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+}
+
+function errorResponse(res, error) {
+  res.status(400).send(error);
+}
+
+app.listen(PORT, () => {
+  console.log(`barcode retriever is running on port ${PORT}`);
+});
